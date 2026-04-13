503e4a17df0f7844491501cb1ae5aea464b708a0
Nikita200 <Ponomariovnikita02@gmail.com>
2026-04-13 18:52:16 +0200

    .

--- quotes_project/quotes/models.py
+++ quotes_project/quotes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    full_name = models.CharField(max_length=100)
+    born_date = models.CharField(max_length=100)
+    born_location = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.fullname
+
+    
+class Tag(models.Model):
+    tag = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.tag
+
+
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.text[:20]
