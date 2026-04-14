3bcf40fe815c1b0b0ab99401475000775147b37e
Nikita200 <Ponomariovnikita02@gmail.com>
2026-04-14 20:46:31 +0200

    we stop on base functions

--- quotes_project/quotes_app/urls.py
+++ quotes_project/quotes_app/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 app_name = "quotes_app"
 
 urlpatterns = [
-    path('', views.main, name="main")
+    path('', views.main, name="main"),
+   
 ]
