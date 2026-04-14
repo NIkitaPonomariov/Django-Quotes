15ebf56bc2ee2da7accc6dfa84516836f001a567
Nikita200 <Ponomariovnikita02@gmail.com>
2026-04-14 19:27:47 +0200

    .

--- quotes_project/quotes/urls.py
+++ quotes_project/quotes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.main, name="main"),
-    path('register/', views.register, name="register"),
+    path('accounts/register/', views.register, name="register"),
     path('add_author/', views.add_author, name="add_author"),
     path('add_quote/', views.add_quote, name="add_quote"),
     path('add_tag/', views.add_tag, name="add_tag"),
--- quotes_project/quotes/views.py
+++ quotes_project/quotes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Quote, Tag, Author
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 def main(request):
@@ -12,28 +14,37 @@ def main(request):
 
 
 def register(request):
-    return render(request, 'quotes/home.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/login/')
+    else:
+        form = UserCreationForm()
 
-def add_author(request):
-    return render(request, 'quotes/home.html')
+    return render(request, 'quotes/register.html', {"form": form})
 
+@login_required
 def add_quote(request):
     if request.method == "POST":
         text = request.POST.get("text")
         author_id = request.POST.get("author")
+        tag_name = request.POST.get("tag")
 
         author = Author.objects.get(id=author_id)
 
-        Quote.objects.create(
+        quote = Quote.objects.create(
             text=text,
             author=author
         )
 
-    authors = Author.objects.all()
+        tag, created = Tag.objects.get_or_create(tag=tag_name)
+        quote.tags.add(tag)
 
+    authors = Author.objects.all()
     return render(request, 'quotes/add_quote.html', {"authors": authors})
 
-
+@login_required
 def add_tag(request):
     return render(request, 'quotes/home.html')
 
@@ -43,3 +54,22 @@ def quotes_by_tag(request):
 def author_detail(request, author_id):
     author = Author.objects.get(id=author_id)
     return render(request, 'quotes/author.html', {"author": author})
+
+@login_required
+def add_author(request):
+    if request.method == "POST":
+        fullname = request.POST.get("fullname")
+        born_date = request.POST.get("born_date")
+        born_location = request.POST.get("born_location")
+        description = request.POST.get("description")
+
+        Author.objects.create(
+            fullname=fullname,
+            born_date=born_date,
+            born_location=born_location,
+            description=description
+        )
+
+        return redirect('/')
+
+    return render(request, 'quotes/add_author.html')
