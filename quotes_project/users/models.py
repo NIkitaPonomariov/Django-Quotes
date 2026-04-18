13916bf2eae019f5e7ce65af67d8cfd9ce2a5b28
Nikita200 <Ponomariovnikita02@gmail.com>
2026-04-18 20:12:43 +0200

    .

--- quotes_project/users/models.py
+++ quotes_project/users/models.py
@@ -1,15 +1,34 @@
 from django.db import models
+from django.utils.html import strip_tags
 from django.contrib.auth.models import AbstractUser, \
     BaseUserManager
-from django.utils.http import strip_tags
  
+
 class CustomUserManager(BaseUserManager):
-    pass
+    #create simple user whiotout extrafields
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('the emails must be')
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
+    #somethiing like register user like admin with difirrent extra fields
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have "is_staff" = True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have "is_staff" = True')
+
+        return self.create_user(email, first_name, last_name, password=None, **extra_fields)
 
 
 class CustomUser(AbstractUser):
+    #our user in db (we using postgresql)
     email = models.EmailField(unique=True, max_length=70)
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
@@ -32,3 +51,10 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+    
+
+
+
+"""
+after creation models we need to setting this custom user
+"""
