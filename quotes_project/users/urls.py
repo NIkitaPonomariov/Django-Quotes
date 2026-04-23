e1cfd28628ff4c31ef033631cdebacb70bb73815
Nikita200 <Ponomariovnikita02@gmail.com>
2026-04-23 18:06:09 +0200

    create ursl file and templates

---
+++ quotes_project/users/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+app_name = "users"
+
+urlpatterns = [
+    path('login/', LoginView.as_view, name="login"),
+    path('logout/', LogoutView.as_view, name="logout"),
+    path('register/', views.main, name="register"),
+]
